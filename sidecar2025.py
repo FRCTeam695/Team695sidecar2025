2b94c8c5128131f911e6c0bc1e6c17509831a9ee
Eva Zheng <eva.zheng2142147@gmail.com>
2025-01-26 11:33:22 -0500

    tkinter formatting

--- sidecar2025.py
+++ sidecar2025.py
@@ -1,5 +1,6 @@
 import tkinter
 import tkinter.font
+import math
 import time
 from networktables import NetworkTables
 
@@ -11,6 +12,16 @@ def buttonPressed(button, place):
     print(place + " chosen")
     button.configure(fg="black", bg="white")
     print("The location is " + place)
+
+# method for hexagon
+def create_hexagon(canvas, x_center, y_center, size):
+    points = []
+    for i in range(6):
+        angle = math.radians(60 * i)
+        x = x_center + size * math.cos(angle)
+        y = y_center + size * math.sin(angle)
+        points.extend([x, y])
+    canvas.create_polygon(points, outline = 'blue', fill = 'white', width = 2)
 
 # Set the RoboRIO IP address (from old nt code)
 ip = "10.6.95.2"
@@ -29,7 +40,14 @@ sidecarTables.putString("scoringLocation", "")
 # new interface window
 window = tkinter.Tk()
 window.title("sidecar695 FRC 2025")
-window.geometry('574x574')
+window.geometry('500x500')
+
+# making the canvas widget
+canvas = tkinter.Canvas(window, width=400, height=400)
+canvas.pack()
+
+# making the hexagon
+create_hexagon(canvas, 200, 250, 150)
 
 # button names
 buttonLabels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
@@ -52,27 +70,31 @@ for idx, label in enumerate(buttonLabels):
         command=lambda b=label: buttonPressed(button, b)  # pass arguments
     )
     #btn.config(command=lambda b=btn, l=label: buttonPressed(b, l))
-    button.place(x=250, y=50 + idx * 40)
+   
     if label=="A":
-        button.place(x=75, y=225)
+        button.place(x=255, y=80)
     if label=="B":
-        button.place(x=75, y=325)
+        button.place(x=185, y=80)
     if label=="C":
-        button.place(x=110, y=400)
+        button.place(x=90, y=120)
     if label=="D":
-        button.place(x=180, y=450)
+        button.place(x=60, y=180)
     if label=="E":
-        button.place(x= 360, y=450)
+        button.place(x=60, y=290)
     if label=="F":
-        button.place(x=410, y=400)
+        button.place(x=90, y=350)
     if label=="G":
-        button.place(x=450, y=325)
+        button.place(x=185, y=390)
     if label=="H":
-        button.place(x=450, y=225)
+        button.place(x=255, y=390)
+    if label=="I":
+        button.place(x=350, y=350)
+    if label=="J":
+        button.place(x=380, y=290)
     if label=="K":
-        button.place(x=180, y=100)
+        button.place(x=380, y=180)
     if label=="L":
-        button.place(x=110, y=150)
+        button.place(x=350, y=120)
 
 # GUI
 window.mainloop()
