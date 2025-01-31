5b1b1ffe96e0d0491f6cba483351a40ae44d6d76
Eva Zheng <eva.zheng2142147@gmail.com>
2025-01-30 19:32:06 -0500

    levels added

--- sidecar2025.py
+++ sidecar2025.py
@@ -6,10 +6,12 @@ from networktables import NetworkTables
 
 # cget = part of tkinter that retrieves information
 def buttonPressed(button, place):
-    print(button.cget("text") + " was chosen at " + place)
     sidecarTables.putString("scoringLocation", place)
     print(place + " chosen")
-    print("The location is " + place)
+
+def levelPressed(btn, level):
+    sidecarTables.putString("levelChosen", level)
+    print(level + " chosen")
 
 # method for hexagon
 def create_hexagon(canvas, x_center, y_center, size):
@@ -34,14 +36,15 @@ else:
 # creates table and sets to none
 sidecarTables = NetworkTables.getTable("sidecarTable")
 sidecarTables.putString("scoringLocation", "")
+sidecarTables.putString("levelChosen", "")
 
 # new interface window
 window = tkinter.Tk()
 window.title("sidecar695 FRC 2025")
-window.geometry('500x500')
+window.geometry('700x500')
 
 # making the canvas widget
-canvas = tkinter.Canvas(window, width=400, height=400)
+canvas = tkinter.Canvas(window, width=600, height=600)
 canvas.pack()
 
 # making the hexagon
@@ -53,7 +56,7 @@ buttonLabels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
 # font type
 button_font = tkinter.font.Font(size=11, weight="bold")
 
-# create each button
+# create each button (letters)
 # idx, label in enumerate(list): = way in python to make loops
 # lambda is needed to pass arguements (when button pressed, do smth)
 for idx, label in enumerate(buttonLabels):
@@ -67,7 +70,6 @@ for idx, label in enumerate(buttonLabels):
         font=button_font,
         command=lambda b=label: buttonPressed(button, b)  # pass arguments
     )
-    #btn.config(command=lambda b=btn, l=label: buttonPressed(b, l))
    
     if label=="A":
         button.place(x=185, y=390)
@@ -93,6 +95,32 @@ for idx, label in enumerate(buttonLabels):
         button.place(x=60, y=290)
     if label=="L":
         button.place(x=90, y=350)
+
+#create each button (levels)
+levelLables = ["L4", "L3", "L2", "L1"]
+level_buttons = []
+
+for idx, label2 in enumerate(levelLables):
+    button2 = tkinter.Button(
+        window,
+        text=label2,
+        width=10,
+        height=2,
+        fg="black",
+        bg="white",
+        font=button_font,
+        command=lambda b2=label2: levelPressed(button2, b2)
+    )
+    level_buttons.append(button2)
+    
+    if label2 == "L4":
+        button2.place(x=550, y=70)
+    if label2 == "L3":
+        button2.place(x=550, y=170)
+    if label2 == "L2":
+        button2.place(x=550, y=270)
+    if label2 == "L1":
+        button2.place(x=550, y=370)
 
 # GUI
 window.mainloop()
