a9ad782c96c9691ad03c62ed2b893a18ce0659a5
Amber Zhang <amberzhang70@gmail.com>
2025-02-05 19:10:44 -0500

    new layout

--- ReefInterface.py
+++ ReefInterface.py
@@ -13,9 +13,9 @@ def buttonPressed(i):
     location = buttonLabels[i]
 
     for butt in buttons:
-        if butt['bg'] == '#00C957':
+        if butt['bg'] == '#FF1493':
             butt['bg'] = 'white'
-    buttons[i].config(bg='#00C957')
+    buttons[i].config(bg='#FF1493')
 
     print(location + " was chosen")
     sidecarTables.putString("scoringLocation", location)
@@ -42,7 +42,7 @@ def gamePieceSelect():
     if currentIntakeMode == 1:
         currentIntakeMode = 2
         gamePiece.config(bg="#00CED1", text="Algae")
-        canvas['bg'] = "#00CED1"
+        canvas['bg'] = "#48D1CC"
 
         for i in range(1, len(buttons), 2):
             buttons[i].config(bg='gray', command=obsolete)
@@ -51,7 +51,7 @@ def gamePieceSelect():
     else:
         currentIntakeMode = 1
         gamePiece.config(bg="#9400D3", text="Coral")
-        canvas['bg'] = "#9400D3"
+        canvas['bg'] = "#ab3fd9"
 
         for i in range(1, len(buttons), 2):
             buttons[i].config(bg='white', command=lambda b=i: buttonPressed(b))
@@ -85,6 +85,19 @@ def combinationater(place, level):
         print("New combination added: " + str(new_combination))
     else:
         print("Combination " + str(new_combination) + " already used")
+
+def reset():
+    for butt in buttons:
+        butt['bg'] = "white"
+    for butt in buttons2:
+        butt['bg'] = "white"
+    
+    currentIntakeMode = 1
+    gamePieceSelect()
+
+    sidecarTables.putString("scoringLocation", "")
+    sidecarTables.putString("scoringLevel", "")
+
 
 def obsolete():
     print("Button obsolete for selected intake mode")
@@ -128,13 +141,13 @@ used_combinations = set()
 
 #window
 window = tk.Tk() #create window
-window.geometry("800x500") #size
-window.title("Reef GUI") #title
+window.geometry("850x500") #size
+window.title("sidecar") #title
 
 #canvas
-canvas = tk.Canvas(window, width = 800, height = 500, bg = '#9400D3')
+canvas = tk.Canvas(window, width = 850, height = 500, bg = '#ab3fd9')
 canvas.place(x=0, y=0)
-canvas.create_polygon((175,120, 325,120, 400,250, 325,380, 175,380, 100,250), fill = '#D1D1D1', outline=G_alliancecolor, width='5') #hexagon
+canvas.create_polygon((300,120, 450,120, 525,250, 450,380, 300,380, 225,250), fill = '#D1D1D1', outline=G_alliancecolor, width='5') #hexagon
 
 #reef buttons
 buttons = []
@@ -153,24 +166,28 @@ while i <= 11:
     buttons.append(button)
     i += 1
 
-buttons[0].place(x=188, y=390, height=50, width=50)
-buttons[1].place(x=262, y=390, height=50, width=50)
-buttons[2].place(x=359, y=350, height=50, width=50)
-buttons[3].place(x=390, y=290, height=50, width=50)
-buttons[4].place(x=390, y=160, height=50, width=50)
-buttons[5].place(x=359, y=100, height=50, width=50)
-buttons[6].place(x=262, y=60, height=50, width=50)
-buttons[7].place(x=188, y=60, height=50, width=50)
-buttons[8].place(x=90, y=100, height=50, width=50)
-buttons[9].place(x=60, y=160, height=50, width=50)
-buttons[10].place(x=60, y=290, height=50, width=50)
-buttons[11].place(x=90, y=350, height=50, width=50)
+buttons[0].place(x=313, y=390, height=50, width=50)
+buttons[1].place(x=387, y=390, height=50, width=50)
+buttons[2].place(x=484, y=350, height=50, width=50)
+buttons[3].place(x=515, y=290, height=50, width=50)
+buttons[4].place(x=515, y=160, height=50, width=50)
+buttons[5].place(x=484, y=100, height=50, width=50)
+buttons[6].place(x=387, y=60, height=50, width=50)
+buttons[7].place(x=313, y=60, height=50, width=50)
+buttons[8].place(x=215, y=100, height=50, width=50)
+buttons[9].place(x=185, y=160, height=50, width=50)
+buttons[10].place(x=185, y=290, height=50, width=50)
+buttons[11].place(x=215, y=350, height=50, width=50)
 
 
 #buttons for selecting level
 gamePiece = tk.Button(window, text="Coral", bg="#9400D3", font=("Book Antiqua", 18))
 gamePiece.config(command=gamePieceSelect)
-gamePiece.place(x=550, y=12, height=75, width=200)
+gamePiece.place(x=50, y=117, height=100, width=100)
+
+resetButton = tk.Button(window, text="Reset", bg="white", font=("Book Antiqua", 18))
+resetButton.config(command=reset)
+resetButton.place(x=50, y=283, height=100, width=100)
 
 buttons2 = []
 j = 0
@@ -184,16 +201,16 @@ while j <= 3:
     buttons2.append(button)
     j += 1
 
-buttons2[3].place(x=550, y=112, height=75, width=200)
+buttons2[3].place(x=600, y=60, height=75, width=200)
 buttons2[3].config(command=lambda: scoringLevel(buttons2[3], "Level 4"))
 
-buttons2[2].place(x=550, y=212, height=75, width=200)
+buttons2[2].place(x=600, y=162, height=75, width=200)
 buttons2[2].config(command=lambda: scoringLevel(buttons2[2], "Level 3"))
 
-buttons2[1].place(x=550, y=312, height=75, width=200)
+buttons2[1].place(x=600, y=263, height=75, width=200)
 buttons2[1].config(command=lambda: scoringLevel(buttons2[1], "Level 2"))
 
-buttons2[0].place(x=550, y=412, height=75, width=200)
+buttons2[0].place(x=600, y=365, height=75, width=200)
 buttons2[0].config(command=lambda: scoringLevel(buttons2[0], "Level 1"))
 
 #run
